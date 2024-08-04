811f58f8ead4ecdc278f8687b20b76eb8911c67a
MGCrafter <xjessyx@nekozdevteam.eu>
2024-08-04 14:30:04 +0200

    Komplett

---
+++ types/directus.d.ts
@@ -0,0 +1,15 @@
+export interface WelcomeMessageData {
+  id?: number;
+  message: string;
+}
+
+export interface HeaderMessageData {
+  id?: number;
+  header: string;
+}
+  
+  export interface LinkData {
+    id?: number;
+    url: string;
+    title: string;
+  }
